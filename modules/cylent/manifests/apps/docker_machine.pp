ebff8239aea6be4f2bc9136fee595bf120646734
Chris Baron <chris.baron@barklyprotects.com>
2015-09-28 17:53:50 -0400

    docker machine orchestration

--- modules/cylent/manifests/apps/docker_machine.pp
+++ modules/cylent/manifests/apps/docker_machine.pp
@@ -2,6 +2,10 @@ class cylent::apps::docker_machine {
   include brewcask
 
   notify { 'class cylent::apps::docker_machine declared': }
+ 
+  $machine_name = "osxdock"
+  $docker_machine_driver = "vmwarefusion"
+  $start_command = "/usr/local/bin/docker-machine start ${machine_name}"
 
   package { 'docker-machine':
     ensure => present,
@@ -9,15 +13,20 @@ class cylent::apps::docker_machine {
   }
  
   exec { 'create machine':
-    command => '/usr/local/bin/docker-machine create osxdock --driver virtualbox',
-    unless => '/usr/local/bin/docker-machine inspect osxdock',
+    command => "/usr/local/bin/docker-machine create $machine_name --driver $docker_machine_driver",
+    unless => "/usr/local/bin/docker-machine inspect $machine_name",
     require => Package['docker-machine']
   }
 
   exec { 'start machine':
-    command =>  '/usr/local/bin/docker-machine start osxdock',
-    unless => 'test Running = `/usr/local/bin/docker-machine status osxdock`',
+    command => $start_command,
+    unless => "test Running = `/usr/local/bin/docker-machine status $machine_name`",
     require => Exec['create machine']
   }
 
+  file { "${home}/Library/LaunchAgents/com.barkly.docker-machine.plist":
+    ensure => file,
+    content => template('cylent/docker-machine.plist.erb'),
+    mode => 0400
+  }
 }
