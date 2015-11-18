87f5f3ccc706880c4ed7954e1e7c818c5c5f8f0d
Chris Baron <chris.baron@barklyprotects.com>
2015-11-18 11:12:21 -0500

    parameterize the docker machine driver, defaults to vmwarefusion

--- modules/cylent/manifests/apps/docker_machine.pp
+++ modules/cylent/manifests/apps/docker_machine.pp
@@ -1,12 +1,12 @@
-class cylent::apps::docker_machine {
+class cylent::apps::docker_machine (
+  $machine_name = "osxdock",
+  $docker_machine_driver = "vmwarefusion",
+  $boot2docker_url = "https://github.com/boot2docker/boot2docker/releases/download/v1.8.2/boot2docker.iso",
+  $start_command = "/usr/local/bin/docker-machine start ${machine_name}"
+) {
   include brewcask
 
   notify { 'class cylent::apps::docker_machine declared': }
- 
-  $machine_name = "osxdock"
-  $docker_machine_driver = "vmwarefusion"
-  $boot2docker_url = "https://github.com/boot2docker/boot2docker/releases/download/v1.8.2/boot2docker.iso"
-  $start_command = "/usr/local/bin/docker-machine start ${machine_name}"
 
   package { 'docker-machine':
     ensure => present,
--- modules/people/manifests/chrisbaron.pp
+++ modules/people/manifests/chrisbaron.pp
@@ -1,5 +1,8 @@
 class people::chrisbaron {
-  include cylent::dev_environment
+  class { 'cylent::dev_environment':
+    docker_machine_driver => "virtualbox"   
+ }
+ #include cylent::dev_environment
 
   package {
             [
