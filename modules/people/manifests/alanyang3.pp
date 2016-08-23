375ae0403970cdb0f2fa0a4fa26a59b7bc07a2d9
Alan Yang <alan.yang@barkly.com>
2016-08-23 10:24:38 -0400

    Add alanyang3 manifest

---
+++ modules/people/manifests/alanyang3.pp
@@ -0,0 +1,21 @@
+class people::default {
+  include cylent::dev_environment
+
+  ###### Environment Settings ##########
+  include osx::dock::autohide
+  include osx::dock::dim_hidden_apps
+  include osx::finder::show_all_on_desktop
+  include osx::finder::empty_trash_securely
+  include osx::finder::show_hidden_files
+
+
+  class { 'osx::dock::hot_corners':
+    top_right => 'Application Windows',
+    top_left  => 'Desktop',
+    bottom_right => 'Start Screen Saver',
+    bottom_left => 'Dashboard'
+  }
+
+  include cylent::osx::dock::minimize_to_application
+
+}
