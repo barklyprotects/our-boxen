3ce0b30f381dfacde3b7cb5ceb73e3712a0456de
Alan Yang <alan.yang@barkly.com>
2016-11-02 14:09:24 -0400

    Clone cuckoo-osx-env recursively

--- modules/projects/manifests/beam.pp
+++ modules/projects/manifests/beam.pp
@@ -2,7 +2,8 @@ class projects::beam {
 
   repository { "${cylent_repo_dir}/cuckoo-osx-env":
     source => 'barklyprotects/cuckoo-osx-env',
-    require => File[$cylent_repo_dir]
+    require => File[$cylent_repo_dir],
+    extra => '--recursive'
   }
 
 }
